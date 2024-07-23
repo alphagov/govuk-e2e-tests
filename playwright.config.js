c23bcf0b4ec03220246998538d7769bc2478673c
Sean Rankine <sean.rankine@me.com>
2024-07-23 15:16:44 +0100

    Set test-results in tmp folder

--- playwright.config.js
+++ playwright.config.js
@@ -10,6 +10,7 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
  */
 export default defineConfig({
   testDir: "./tests",
+  outputDir: "tmp/test-results",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
