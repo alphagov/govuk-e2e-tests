93dfb0c4358637d44fe5da3bfcac35070f522cda
Sean Rankine <sean.rankine@me.com>
2024-07-26 13:43:01 +0100

    Fix linting issues

--- tests/bouncer.spec.js
+++ tests/bouncer.spec.js
@@ -2,9 +2,7 @@ import { expect } from "@playwright/test";
 import { test } from "../lib/cachebust-test";
 
 test.describe("Bouncer", () => {
-  test("Check redirects work for transitioned sites",
-    { tag: ["@local-network"] },
-    async ({ request }) => {
+  test("Check redirects work for transitioned sites", { tag: ["@local-network"] }, async ({ request }) => {
     const response = await request.get("http://www.attorneygeneral.gov.uk", { maxRedirects: 0 });
     expect(response.status()).toBe(301);
     expect(response.headers()["location"]).toBe("https://www.gov.uk/government/organisations/attorney-generals-office");
