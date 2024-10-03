75ac0ce3293631d75e978a30f84ba1215d615e1b
Sean Rankine <sean.rankine@me.com>
2024-10-03 11:32:23 +0100

    Add govuk-chat tests

---
+++ tests/chat.spec.js
@@ -0,0 +1,19 @@
+import { expect } from "@playwright/test";
+import { test } from "../lib/cachebust-test";
+import { publishingAppUrl } from "../lib/utils";
+
+test.describe("GOV.UK Chat", { tag: ["@app-govuk-chat"] }, () => {
+  test("Can view a static page", async ({ page }) => {
+    await page.goto("/chat/about");
+    await expect(page.getByRole("heading", { name: "About GOV.UK Chat" })).toBeVisible();
+  });
+});
+
+test.describe("GOV.UK Chat Admin", { tag: ["@app-govuk-chat"] }, () => {
+  test.use({ baseURL: publishingAppUrl("chat") });
+
+  test("Can log in to chat admin", async ({ page }) => {
+    await page.goto("/admin");
+    await expect(page.getByRole("heading", { name: "GOV.UK Chat Admin" })).toBeVisible();
+  });
+});
