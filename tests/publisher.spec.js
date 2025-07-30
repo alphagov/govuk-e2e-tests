a6117fe6243dbc0e9e84f7f3058af0eb4b0aba40
Jonathon <jonathon.shire@digital.cabinet-office.gov.uk>
2025-07-30 16:18:32 +0100

    Update publisher e2e to look for correct element
    Previous attempt at heading did not cover all bases, so trying it with
    a link target instead.

--- tests/publisher.spec.js
+++ tests/publisher.spec.js
@@ -8,7 +8,7 @@ test.describe("Publisher", { tag: ["@app-publisher"] }, () => {
 
   test("Can log in to Publisher", { tag: ["@app-publishing-api", "@publishing-app"] }, async ({ page }) => {
     await page.goto("/");
-    await expect(page.getByRole("heading", { name: "Publications" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Publications" })).toBeVisible();
     await expect(page.locator("#publication-list-container")).toBeVisible();
   });
 
