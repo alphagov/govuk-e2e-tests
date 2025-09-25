712943fe6b105e37c079243fd79b1f38a39a0202
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-25 14:02:35 +0100

    Remove email alert frontend test
    Consultation pages will be rendered by Frontend.
    Call for evidence and publication pages also
    render the Get email links, but have already
    been moved from government-frontend. There
    is already a Frontend test in place.

--- tests/government-frontend.spec.js
+++ tests/government-frontend.spec.js
@@ -13,10 +13,4 @@ test.describe("Government Frontend", { tag: ["@app-government-frontend"] }, () =
       )
     ).toBeVisible();
   });
-
-  test("Check links to Email Alert Frontend work", { tag: ["@app-email-alert-frontend"] }, async ({ page }) => {
-    await page.goto("/government/consultations/soft-drinks-industry-levy");
-    await page.getByRole("button", { name: "Get emails about this page" }).first().click();
-    await expect(page.getByText("You need a GOV.UK One Login to get these emails.")).toBeVisible();
-  });
 });
