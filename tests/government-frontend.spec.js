4496c00586727b50f2a6e3713a3aea8d3de2291e
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2025-10-16 16:00:11 +0100

    Update page used in government-frontend
    Update the page tested to a world organisation page as topical_events has moved from `government-frontend` to `frontend`
    
    The `level: 1` option is added to ensure the test works as expected as there are 2 heading on this page with the same text but different heading levels.

--- tests/government-frontend.spec.js
+++ tests/government-frontend.spec.js
@@ -3,19 +3,15 @@ import { test } from "../lib/cachebust-test";
 
 test.describe("Government Frontend", { tag: ["@app-government-frontend"] }, () => {
   test("Check the frontend can talk to Content Store", { tag: ["@worksonmirror"] }, async ({ page }) => {
-    await page.goto("/government/topical-events/2014-overseas-territories-joint-ministerial-council/about");
+    await page.goto("/world/organisations/british-embassy-madrid");
+    await expect(page.getByRole("heading", { name: "British Embassy Madrid", level: 1 })).toBeVisible();
     await expect(
-      page.getByRole("heading", { name: "Priorities for the Overseas Territories Joint Ministerial Council 2014" })
-    ).toBeVisible();
-    await expect(
-      page.getByText(
-        "The UK and the Overseas Territories share a vision for the Territories as vibrant and flourishing communities"
-      )
+      page.getByText("The British Embassy in Madrid maintains and develops relations between the UK and Spain.")
     ).toBeVisible();
   });
 
   test("check application CSS loads", async ({ page, request }) => {
-    await page.goto("/government/topical-events/2014-overseas-territories-joint-ministerial-council/about");
+    await page.goto("/world/organisations/british-embassy-madrid");
     const applicationCSSPath = await page
       .locator('link[rel="stylesheet"][href*="/assets/government-frontend/application-"]')
       .getAttribute("href");
