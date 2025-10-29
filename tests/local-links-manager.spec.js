a544aa9c978be4bda5d2d990984f03a446577f7b
deborahchua <19667619+deborahchua@users.noreply.github.com>
2025-10-29 10:43:11 +0000

    Revert "[PNP-9662] Disable local-links-manager e2e tests in Staging"

--- tests/local-links-manager.spec.js
+++ tests/local-links-manager.spec.js
@@ -5,13 +5,9 @@ import { publishingAppUrl } from "../lib/utils";
 test.describe("Local Links Manager", { tag: ["@app-local-links-manager"] }, () => {
   test.use({ baseURL: publishingAppUrl("local-links-manager") });
 
-  test(
-    "Can log in to Local Links Manager",
-    { tag: ["@app-publishing-api", "@publishing-app", "@not-staging"] },
-    async ({ page }) => {
-      await page.goto("/");
-      await expect(page.getByText("Local Links Manager")).toBeVisible();
-      await expect(page.getByText("Councils")).toBeVisible();
-    }
-  );
+  test("Can log in to Local Links Manager", { tag: ["@app-publishing-api", "@publishing-app"] }, async ({ page }) => {
+    await page.goto("/");
+    await expect(page.getByText("Local Links Manager")).toBeVisible();
+    await expect(page.getByText("Councils")).toBeVisible();
+  });
 });
