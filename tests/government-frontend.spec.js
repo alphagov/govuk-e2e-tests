d1221694e4279b7a719b944b4e4da19030f26301
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-18 11:52:20 +0000

    Update government-frontend e2e tests
    - hmrc_manual_sections are about to be migrated, so move to hmrc_manual

--- tests/government-frontend.spec.js
+++ tests/government-frontend.spec.js
@@ -3,7 +3,7 @@ import { test } from "../lib/cachebust-test";
 
 test.describe("Government Frontend", { tag: ["@app-government-frontend"] }, () => {
   test("Check the frontend can talk to Content Store", { tag: ["@worksonmirror"] }, async ({ page }) => {
-    await page.goto("/hmrc-internal-manuals/apprenticeship-levy/alm09000");
+    await page.goto("/hmrc-internal-manuals/apprenticeship-levy");
     await expect(
       page.getByRole("heading", { name: "ALM09000 - Paying and reporting the levy: contents", level: 1 })
     ).toBeVisible();
@@ -11,7 +11,7 @@ test.describe("Government Frontend", { tag: ["@app-government-frontend"] }, () =
   });
 
   test("check application CSS loads", async ({ page, request }) => {
-    await page.goto("/hmrc-internal-manuals/apprenticeship-levy/alm09000");
+    await page.goto("/hmrc-internal-manuals/apprenticeship-levy");
     const applicationCSSPath = await page
       .locator('link[rel="stylesheet"][href*="/assets/government-frontend/application-"]')
       .getAttribute("href");
